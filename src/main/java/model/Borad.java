1d500808d5d6f459b97c4afeeb5e1c3ee2271870
Duaa3 <Duaahil44@gmail.com>
2023-03-12 09:14:18 +0400

    botAPI

--- src/main/java/model/Borad.java
+++ src/main/java/model/Borad.java
@@ -4,17 +4,18 @@ public class Borad {
     private char[][] board;
 
     public Borad() {
+        // Define a no-argument constructor that initializes the board to an empty 3x3 array
         this.board = new char[][]{{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
     }
-
+    // Define a constructor that takes a 2D char array as an argument and sets the board to it
     public  Borad(char[][] board) {
         this.board = board;
     }
-
+    // Define a getter method for the board
     public char[][] getBoard() {
         return board;
     }
-
+    // Define a setter method for the board
     public void setBoard(char[][] board) {
         this.board = board;
     }
--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -8,16 +8,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api/bot")
 public class Controller {
     @PostMapping
+    // Map the request parameters to the boardString and symbol variables
     public Borad getBoard(@RequestParam("board") String boardString, @RequestParam("symbol") char symbol) {
+        // Convert the boardString parameter to a 2D char array
         char[][] board = new char[3][3];
         for (int i = 0; i < 9; i++) {
             int row = i / 3;
             int col = i % 3;
             board[row][col] = boardString.charAt(i);
         }
+        // Create a new bot with the specified symbol and make a move on the board
         Bot bot = new Bot(symbol);
         int[] move = bot.makeMove(board);
         board[move[0]][move[1]] = symbol;
+        // Return the updated board as a Borad object
         return new Borad(board);
     }
 
